e6d6c34886e66d5eded0d9a82f2e7d10cb1b3a2b
Rhythm Saha <rs.2001.saha@gmail.com>
2024-03-09 22:05:54 +0530

    Update user authentication logic***

---
+++ lib/FilterOptions.ts
@@ -0,0 +1,634 @@
+const FilterOptions = {
+    colors: [
+        {
+            _id: "659310aed014a5aa1a02b669",
+            name: "Black",
+            hex: "#000000",
+        },
+        {
+            _id: "659310aed014a5aa1a02b694",
+            name: "Yellow",
+            hex: "#FFFF00",
+        },
+        {
+            _id: "659310aed014a5aa1a02b67b",
+            name: "Light Pink",
+            hex: "#FFB6C1",
+        },
+        {
+            _id: "659310aed014a5aa1a02b687",
+            name: "Off White",
+            hex: "#FAFAD2",
+        },
+        {
+            _id: "659310aed014a5aa1a02b68b",
+            name: "Pistachio",
+            hex: "#93C572",
+        },
+        {
+            _id: "659310aed014a5aa1a02b678",
+            name: "Light Blue",
+            hex: "#ADD8E6",
+        },
+        {
+            _id: "659310aed014a5aa1a02b68a",
+            name: "Pink",
+            hex: "#FFC0CB",
+        },
+        {
+            _id: "659310aed014a5aa1a02b674",
+            name: "Gold",
+            hex: "#FFD700",
+        },
+        {
+            _id: "659310aed014a5aa1a02b692",
+            name: "White",
+            hex: "#FFFFFF",
+        },
+        {
+            _id: "659310aed014a5aa1a02b68d",
+            name: "Red",
+            hex: "#FF0000",
+        },
+        {
+            _id: "659310aed014a5aa1a02b677",
+            name: "Light Beige",
+            hex: "#F5F5DC",
+        },
+        {
+            _id: "659310aed014a5aa1a02b66c",
+            name: "Cream",
+            hex: "#FFFDD0",
+        },
+        {
+            _id: "659310aed014a5aa1a02b689",
+            name: "Orange",
+            hex: "#FFA500",
+        },
+        {
+            _id: "659310aed014a5aa1a02b67c",
+            name: "Light Red",
+            hex: "#FF4500",
+        },
+        {
+            _id: "659310aed014a5aa1a02b675",
+            name: "Green",
+            hex: "#008000",
+        },
+        {
+            _id: "659310aed014a5aa1a02b690",
+            name: "Violet",
+            hex: "#EE82EE",
+        },
+        {
+            _id: "659310aed014a5aa1a02b66e",
+            name: "Dark Green",
+            hex: "#006400",
+        },
+        {
+            _id: "659310aed014a5aa1a02b670",
+            name: "Dark Pink",
+            hex: "#FF1493",
+        },
+        {
+            _id: "659310aed014a5aa1a02b68c",
+            name: "Purple",
+            hex: "#800080",
+        },
+        {
+            _id: "659310aed014a5aa1a02b671",
+            name: "Dark Red",
+            hex: "#8B0000",
+        },
+        {
+            _id: "659310aed014a5aa1a02b676",
+            name: "Grey",
+            hex: "#808080",
+        },
+        {
+            _id: "659310aed014a5aa1a02b66d",
+            name: "Dark Blue",
+            hex: "#00008B",
+        },
+        {
+            _id: "659310aed014a5aa1a02b672",
+            name: "Dark Violet",
+            hex: "#9400D3",
+        },
+        {
+            _id: "659310aed014a5aa1a02b67a",
+            name: "Light Orange",
+            hex: "#FFD700",
+        },
+        {
+            _id: "659310aed014a5aa1a02b67d",
+            name: "Light Violet",
+            hex: "#DDA0DD",
+        },
+        {
+            _id: "659310aed014a5aa1a02b673",
+            name: "Dark Yellow",
+            hex: "#B8860B",
+        },
+        {
+            _id: "659310aed014a5aa1a02b686",
+            name: "Ocean Green",
+            hex: "#3CB371",
+        },
+        {
+            _id: "659310aed014a5aa1a02b67e",
+            name: "Light Yellow",
+            hex: "#FFFFE0",
+        },
+        {
+            _id: "659310aed014a5aa1a02b679",
+            name: "Light Green",
+            hex: "#90EE90",
+        },
+        {
+            _id: "659310aed014a5aa1a02b67f",
+            name: "Lime",
+            hex: "#00FF00",
+        },
+        {
+            _id: "659310aed014a5aa1a02b66f",
+            name: "Dark Orange",
+            hex: "#FF8C00",
+        },
+        {
+            _id: "659310aed014a5aa1a02b68f",
+            name: "Turquoise",
+            hex: "#40E0D0",
+        },
+        {
+            _id: "659310aed014a5aa1a02b66b",
+            name: "Brown",
+            hex: "#A52A2A",
+        },
+        {
+            _id: "659310aed014a5aa1a02b668",
+            name: "Beige",
+            hex: "#F5F5DC",
+        },
+        {
+            _id: "659310aed014a5aa1a02b680",
+            name: "Maroon",
+            hex: "#800000",
+        },
+        {
+            _id: "659310aed014a5aa1a02b685",
+            name: "Navy",
+            hex: "#000080",
+        },
+        {
+            _id: "659310aed014a5aa1a02b684",
+            name: "Mustard",
+            hex: "#FFFF00",
+        },
+        {
+            _id: "659310aed014a5aa1a02b66a",
+            name: "Blue",
+            hex: "#0000FF",
+        },
+    ],
+    collections: [
+        {
+            _id: "659304072784df2d7ed9253d",
+            name: "Chikankari Sarees",
+        },
+        {
+            _id: "65930e7b2784df2d7ed92647",
+            name: "Gotapatti Sarees",
+        },
+        {
+            _id: "65930ed62784df2d7ed926a2",
+            name: "Woven Sarees",
+        },
+        {
+            _id: "65930e972784df2d7ed92671",
+            name: "Mukaish Sarees",
+        },
+        {
+            _id: "65930e882784df2d7ed9265c",
+            name: "Bagru Sarees",
+        },
+        {
+            _id: "65930eb82784df2d7ed92694",
+            name: "Block Printed sarees",
+        },
+        {
+            _id: "659308292784df2d7ed925bd",
+            name: "Ajrakh Sarees",
+        },
+        {
+            _id: "65e208bc4912d29385d26a9a",
+            name: "Woven Sarees",
+        },
+        {
+            _id: "659307892784df2d7ed92570",
+            name: "Pochampalli Sarees",
+        },
+        {
+            _id: "65930ee62784df2d7ed926a9",
+            name: "Tie & Dye Sarees",
+        },
+        {
+            _id: "65930e832784df2d7ed92655",
+            name: "Leheriya Sarees",
+        },
+        {
+            _id: "65930ea42784df2d7ed92686",
+            name: "Sanganeri Sarees",
+        },
+        {
+            _id: "659307462784df2d7ed92554",
+            name: "Bhagalpuri Sarees",
+        },
+        {
+            _id: "65930e922784df2d7ed9266a",
+            name: "Shibori Sarees",
+        },
+        {
+            _id: "659303d92784df2d7ed9251d",
+            name: "Maheshwari Sarees",
+        },
+        {
+            _id: "65930e772784df2d7ed92640",
+            name: "Bandhani Sarees",
+        },
+        {
+            _id: "659303d32784df2d7ed92516",
+            name: "Chanderi Sarees",
+        },
+        {
+            _id: "65930ea02784df2d7ed9267f",
+            name: "Dabu Sarees",
+        },
+        {
+            _id: "65930e722784df2d7ed92639",
+            name: "Brocade Sarees",
+        },
+        {
+            _id: "659307c72784df2d7ed9258b",
+            name: "Soft Silk Sarees",
+        },
+        {
+            _id: "659307362784df2d7ed92546",
+            name: "Bengali Sarees",
+        },
+        {
+            _id: "659307812784df2d7ed92569",
+            name: "Kalamkari Sarees",
+        },
+        {
+            _id: "65930ebf2784df2d7ed9269b",
+            name: "Embroidery Sarees",
+        },
+        {
+            _id: "659303e92784df2d7ed9252b",
+            name: "Batik Sarees",
+        },
+        {
+            _id: "65930e7f2784df2d7ed9264e",
+            name: "Ikat Sarees",
+        },
+        {
+            _id: "659300dc6933dc3de5e5c7a3",
+            name: "Banarasi Sarees",
+        },
+        {
+            _id: "659307752784df2d7ed92562",
+            name: "Kanjivaram Sarees",
+        },
+        {
+            _id: "6593073a2784df2d7ed9254d",
+            name: "Jamdani Sarees",
+        },
+    ],
+    attributes: [
+        {
+            _id: "657079723d9601c4f9cb5ec4",
+            name: "Craft",
+            subcategories: [
+                {
+                    _id: "65931acd446f26915c92738f",
+                    name: "Ajrakh",
+                },
+                {
+                    _id: "65931acd446f26915c9273a4",
+                    name: "Shibori",
+                },
+                {
+                    _id: "65931acd446f26915c927396",
+                    name: "Cutwork",
+                },
+                {
+                    _id: "65931acd446f26915c92739a",
+                    name: "Hand Painted",
+                },
+                {
+                    _id: "65931acd446f26915c927397",
+                    name: "Embroidered",
+                },
+                {
+                    _id: "65931acd446f26915c9273a6",
+                    name: "Tie and Dye",
+                },
+                {
+                    _id: "65931acd446f26915c9273a0",
+                    name: "Lehariya",
+                },
+                {
+                    _id: "65931acd446f26915c927391",
+                    name: "Batik",
+                },
+                {
+                    _id: "65931acd446f26915c9273a7",
+                    name: "Woven",
+                },
+                {
+                    _id: "65931acd446f26915c9273a2",
+                    name: "Printed",
+                },
+                {
+                    _id: "65931acd446f26915c92739f",
+                    name: "Kalamkari",
+                },
+                {
+                    _id: "65931acd446f26915c9273a8",
+                    name: "Woven Design",
+                },
+                {
+                    _id: "659320eee600b8b93d027f63",
+                    name: "Ikat",
+                },
+                {
+                    _id: "65931acd446f26915c927394",
+                    name: "Brocade",
+                },
+                {
+                    _id: "65931acd446f26915c92739d",
+                    name: "Jamdani",
+                },
+                {
+                    _id: "65931acd446f26915c9273a3",
+                    name: "Sanganeri",
+                },
+                {
+                    _id: "65931acd446f26915c927393",
+                    name: "Block Printed",
+                },
+                {
+                    _id: "65931acd446f26915c927399",
+                    name: "Gota Patti",
+                },
+                {
+                    _id: "65931acd446f26915c92739e",
+                    name: "Kadwa",
+                },
+                {
+                    _id: "65931acd446f26915c927395",
+                    name: "Chikankari",
+                },
+            ],
+        },
+        {
+            _id: "65931152441a1a934e178a92",
+            name: "Zari",
+            subcategories: [
+                {
+                    _id: "659321329f3cdf61df4f38fe",
+                    name: "Pure Zari",
+                },
+                {
+                    _id: "659321369f3cdf61df4f3905",
+                    name: "Tested Zari",
+                },
+                {
+                    _id: "65e1fa1b4912d29385d26a80",
+                    name: "Without Zari",
+                },
+                {
+                    _id: "6593212a9f3cdf61df4f38f7",
+                    name: "Faux Zari",
+                },
+            ],
+        },
+        {
+            _id: "657070933d9601c4f9cb5ebe",
+            name: "Occassion",
+            subcategories: [
+                {
+                    _id: "65737b958c8011c9be0b17b6",
+                    name: "Party Wear",
+                },
+                {
+                    _id: "65931b971b495bff09fb1de7",
+                    name: "Casual Wear",
+                },
+                {
+                    _id: "65931bae1b495bff09fb1dfc",
+                    name: "Traditional Wear",
+                },
+                {
+                    _id: "6574a0f9f4d661716ad959e6",
+                    name: "Bridal",
+                },
+                {
+                    _id: "65737b657c5a3cb5bae715ef",
+                    name: "Festival",
+                },
+                {
+                    _id: "65931ba51b495bff09fb1df5",
+                    name: "Formal Wear",
+                },
+                {
+                    _id: "65737b5d7c5a3cb5bae715e5",
+                    name: "Wedding",
+                },
+            ],
+        },
+        {
+            _id: "657070813d9601c4f9cb5ebb",
+            name: "Fabric",
+            subcategories: [
+                {
+                    _id: "659308eb2784df2d7ed925d0",
+                    name: "Crepe",
+                },
+                {
+                    _id: "6593090c2784df2d7ed92601",
+                    name: "Mulmul",
+                },
+                {
+                    _id: "6593091a2784df2d7ed92616",
+                    name: "Tencel",
+                },
+                {
+                    _id: "659308ee2784df2d7ed925d7",
+                    name: "Eri",
+                },
+                {
+                    _id: "659309252784df2d7ed9262b",
+                    name: "Viscose",
+                },
+                {
+                    _id: "659309042784df2d7ed925fa",
+                    name: "Muga",
+                },
+                {
+                    _id: "657379ec7c5a3cb5bae715b3",
+                    name: "Linen",
+                },
+                {
+                    _id: "657378dc7c5a3cb5bae7156f",
+                    name: "Chiffon",
+                },
+                {
+                    _id: "657348b78840612d2a0a8c6a",
+                    name: "Cotton",
+                },
+                {
+                    _id: "659309132784df2d7ed92608",
+                    name: "Cotton Silk",
+                },
+                {
+                    _id: "659308fa2784df2d7ed925ec",
+                    name: "Luxe",
+                },
+                {
+                    _id: "657378977c5a3cb5bae71565",
+                    name: "Georgette",
+                },
+                {
+                    _id: "659308f62784df2d7ed925e5",
+                    name: "Kota",
+                },
+                {
+                    _id: "659308ff2784df2d7ed925f3",
+                    name: "Modal",
+                },
+                {
+                    _id: "659308f32784df2d7ed925de",
+                    name: "Khadi",
+                },
+                {
+                    _id: "65737ab17c5a3cb5bae715bd",
+                    name: "Organza",
+                },
+                {
+                    _id: "659309172784df2d7ed9260f",
+                    name: "Tissue",
+                },
+                {
+                    _id: "6593091e2784df2d7ed9261d",
+                    name: "Tussar",
+                },
+            ],
+        },
+        {
+            _id: "6593112d441a1a934e178a87",
+            name: "Origin",
+            subcategories: [
+                {
+                    _id: "659320eee600b8b93d027f73",
+                    name: "Ujjain",
+                },
+                {
+                    _id: "659320eee600b8b93d027f5d",
+                    name: "Bengal",
+                },
+                {
+                    _id: "659320eee600b8b93d027f71",
+                    name: "South",
+                },
+                {
+                    _id: "659320eee600b8b93d027f62",
+                    name: "Gujarat",
+                },
+                {
+                    _id: "659320eee600b8b93d027f68",
+                    name: "Maheshwar",
+                },
+                {
+                    _id: "659320eee600b8b93d027f5f",
+                    name: "Chanderi",
+                },
+                {
+                    _id: "659320eee600b8b93d027f60",
+                    name: "Chattisgarh",
+                },
+                {
+                    _id: "659320eee600b8b93d027f69",
+                    name: "Mangalgiri",
+                },
+                {
+                    _id: "659320eee600b8b93d027f5e",
+                    name: "Bhagalpur",
+                },
+                {
+                    _id: "659320eee600b8b93d027f6c",
+                    name: "Odisha",
+                },
+                {
+                    _id: "659320eee600b8b93d027f61",
+                    name: "Contemporary",
+                },
+                {
+                    _id: "659320eee600b8b93d027f63",
+                    name: "Ikat",
+                },
+                {
+                    _id: "659320eee600b8b93d027f5c",
+                    name: "Banaras",
+                },
+                {
+                    _id: "659320eee600b8b93d027f72",
+                    name: "Srikalahasti",
+                },
+                {
+                    _id: "659320eee600b8b93d027f6e",
+                    name: "Pochampalli",
+                },
+                {
+                    _id: "659320eee600b8b93d027f5b",
+                    name: "Bagru",
+                },
+                {
+                    _id: "659320eee600b8b93d027f66",
+                    name: "Machilipatnam",
+                },
+                {
+                    _id: "659320eee600b8b93d027f5a",
+                    name: "Bagh",
+                },
+                {
+                    _id: "659320eee600b8b93d027f6b",
+                    name: "Narayanpet",
+                },
+                {
+                    _id: "659320eee600b8b93d027f58",
+                    name: "Andhra and Telegana",
+                },
+                {
+                    _id: "659320eee600b8b93d027f65",
+                    name: "Lucknow",
+                },
+                {
+                    _id: "659320eee600b8b93d027f6f",
+                    name: "Rajasthan",
+                },
+                {
+                    _id: "659320eee600b8b93d027f59",
+                    name: "Assam",
+                },
+                {
+                    _id: "659320eee600b8b93d027f64",
+                    name: "Kanjivaram",
+                },
+            ],
+        },
+    ],
+};
+
+export default FilterOptions;
