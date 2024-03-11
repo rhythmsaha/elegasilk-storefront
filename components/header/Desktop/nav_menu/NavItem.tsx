6fb990a88f2474934ffbe1886596975896403dc8
Rhythm Saha <rs.2001.saha@gmail.com>
2024-03-11 17:03:39 +0530

    Refactor NavItem component to improve readability and maintainability

--- components/header/Desktop/nav_menu/NavItem.tsx
+++ components/header/Desktop/nav_menu/NavItem.tsx
@@ -10,23 +10,35 @@ const NavItem: FC<INavLink> = ({ path, title, hasSubmenu, subMenu }) => {
 
     if (hasSubmenu === "SINGLE") {
         return (
-            <li className="relative" onMouseEnter={() => setIsShowing(true)} onMouseLeave={() => setIsShowing(false)}>
+            <li
+                className="relative"
+                onMouseEnter={() => setIsShowing(true)}
+                onMouseLeave={() => setIsShowing(false)}
+            >
                 <NavMenuButton title={title} />
-                {isShowing && subMenu && subMenu.length > 0 && <SingleSubMenu links={subMenu as ISubmenu[]} title={title} />}
+                {isShowing && subMenu && subMenu.length > 0 && (
+                    <SingleSubMenu links={subMenu as ISubmenu[]} title={title} />
+                )}
             </li>
         );
     }
 
     if (hasSubmenu === "GROUPED") {
         return (
-            <li className="relative" onMouseEnter={() => setIsShowing(true)} onMouseLeave={() => setIsShowing(false)}>
+            <li
+                className="relative"
+                onMouseEnter={() => setIsShowing(true)}
+                onMouseLeave={() => setIsShowing(false)}
+            >
                 <NavMenuButton title={title} />
-                {isShowing && <GroupedSubMenu title={title} menuGroups={subMenu as ISubmenuGroup[]} />}
+                {isShowing && (
+                    <GroupedSubMenu title={title} menuGroups={subMenu as ISubmenuGroup[]} />
+                )}
             </li>
         );
     }
 
-    return <Navlink href="" title={title} />;
+    return <Navlink href={path || "/"} title={title} />;
 };
 
 export default NavItem;
